331e568376f45c031aeb3e5d28c4c9ec7cb69e2c
harshithauk <harshithauk1500@gmail.com>
2022-03-23 21:39:09 +0530

    Add logic to move 4 balls at different speeds

--- src/main/java/TryProcessing.java
+++ src/main/java/TryProcessing.java
@@ -2,9 +2,10 @@ import processing.core.PApplet;
 
 public class TryProcessing extends PApplet {
 
-
     public static final int WIDTH = 640;
     public static final int HEIGHT = 400;
+    public static final float DIAMETER = 20;
+    public static float xCoordinate1 = 0;
 
     public static void main(String[] args) {
         String[] appletArgs = new String[]{"TryProcessing"};
@@ -17,5 +18,13 @@ public class TryProcessing extends PApplet {
         size(WIDTH, HEIGHT);
     }
 
+    @Override
+    public void draw() {
+        ellipse(xCoordinate1, (float) (HEIGHT * (1.0 / 5)), DIAMETER, DIAMETER);
+        ellipse(xCoordinate1 * 2, (float) (HEIGHT * (2.0 / 5)), DIAMETER, DIAMETER);
+        ellipse(xCoordinate1 * 3, (float) (HEIGHT * (3.0 / 5)), DIAMETER, DIAMETER);
+        ellipse(xCoordinate1 * 4, (float) (HEIGHT * (4.0 / 5)), DIAMETER, DIAMETER);
 
+        xCoordinate1 += 1;
+    }
 }
